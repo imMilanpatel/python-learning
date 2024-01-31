aa20bc1c7210b5761ea74fff91222637ac38d0a8
imMilanPatel <milanpatel3116@gmail.com>
2024-01-31 10:09:22 +0530

    Find Common Element Array File modified a bit.

--- Find_Common_Elements_Array.py
+++ Find_Common_Elements_Array.py
@@ -1,4 +1,4 @@
-"""
+'''
 Find the common Elements in an Array.
 
 Array_1 = [1,2,3,2,1]
@@ -8,4 +8,19 @@ Array_3 = [1,2,3,4]
 Output should be.,
 [1,2,3]
 
-"""
+Array can be of any size and by default there should be two arrays for comparison.
+
+'''
+
+# Milan Patel version of the solution
+# Version 1, using "set".
+Array_1 = [1,2,3,2,1]
+Array_2 = [1,2,3]
+Array_3 = [1,2,3,4]
+
+common_elements = set(Array_1) & set(Array_2) & set(Array_3)
+print(list(common_elements))
+
+
+
+
