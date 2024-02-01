41ddee2c37e24ce78ffe2ad16f939ea797d1fdcb
imMilanPatel <milanpatel3116@gmail.com>
2024-02-01 18:53:42 +0530

    Second file added

---
+++ Second_Largest_Smallest_in_Array.py
@@ -0,0 +1,16 @@
+'''
+Code to find the second largest and Second Smallest number in an Array!
+'''
+
+# Milan Patel's versions.
+# Version 1
+
+def second_largest(number):
+    pass
+
+def second_smallest(number):
+    pass
+
+
+# Inputs
+number_array = [50,8,75,2,95]
