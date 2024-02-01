8c181ddbd3ddeb8305fe7a90df40131dcdc3f94b
imMilanPatel <milanpatel3116@gmail.com>
2024-02-01 18:41:40 +0530

    Version 2 code implemented

--- Find_Common_Elements_Array.py
+++ Find_Common_Elements_Array.py
@@ -14,14 +14,41 @@ Array can be of any size and by default there should be two arrays for compariso
 
 # Milan Patel version of the solution
 # Version 1, using "set".
-Array_1 = [1,2,3,2,1]
+'''Array_1 = [1,2,3,2,1]
 Array_2 = [1,2,3]
-Array_3 = [1,2,3,4]
+Array_3 = [1,2,3,4]'''
 
-common_elements = set(Array_1) & set(Array_2) & set(Array_3)
-print(list(common_elements))
+# common_elements = set(Array_1) & set(Array_2) & set(Array_3)
+# print(list(common_elements))
 
 # Version 2
+
+def find_common_elements(arr1, arr2, arr3):
+    common_elements = []
+
+    for elem1 in arr1:
+        if elem1 in arr2 and elem1 in arr3 and elem1 not in common_elements:
+            common_elements.append(elem1)
+
+    for elem2 in arr2:
+        if elem2 in arr1 and elem2 in arr3 and elem2 not in common_elements:
+            common_elements.append(elem2)
+
+    for elem3 in arr3:
+        if elem3 in arr1 and elem3 in arr2 and elem3 not in common_elements:
+            common_elements.append(elem3)
+
+    return common_elements
+
+# Example usage:
+Array_1 = [1, 2, 3, 2, 1]
+Array_2 = [1, 2, 3]
+Array_3 = [1, 2, 3, 4]
+
+result = find_common_elements(Array_1, Array_2, Array_3)
+print(result)
+
+
 
 
 
