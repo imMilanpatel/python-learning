93819c8e8c77003f893c94faf5b5e5c8ee101e75
imMilanPatel <milanpatel3116@gmail.com>
2024-02-07 13:21:28 +0530

    Challenge 1 completed

--- Strings basics.py
+++ Strings basics.py
@@ -5,8 +5,36 @@ Solutions will be based on the challenges mentioned in the above repo only.
 
 '''
 
-# 1. Create a string & print out each character individually.
+# Challenge:1. Create a string & print out each character individually.
+
+# Method to printout each character of a string individually.
+'''
+# Using for loop
+def each_character(string):
+    for character in string:
+        print(character)
+'''
+# Using Join Function
+def each_character(string):
+    print('\n'.join(string))
+
+# Input String for the challenge 1
+input_string = "Hello Python learner"
+each_character(input_string)
+
+# Input String for the challenge 1
+input_string = "Hello Python learner"
+each_character(input_string)
+
+#########################################################
+
+
 # 2. Create a string & find the length of it.
+
+
+
+
+
 # 3. Print a string with different predefined variables in it.
 # 4. Write a program that adds 'ing' to every string.
 # 5. Write a program that replaces every 's' in a string with a '$'
