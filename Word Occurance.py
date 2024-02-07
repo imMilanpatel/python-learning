ecdd6ad0aa61a1c3df05d12df748257816816ee3
imMilanPatel <milanpatel3116@gmail.com>
2024-02-07 09:42:43 +0530

    Problem statment drafted

---
+++ Word Occurance.py
@@ -0,0 +1,10 @@
+'''
+Goal is print the following with the a statement as input.
+
+Input: I am a Python Developer, I am proud of it.
+
+Output: Python: 1, Proud: 1, of: 1, i: 2, developer: 1, it: 1, am: 2. 
+
+Note: Output string can be of any order; need not be same as what is shown in above output statement.
+
+'''
