3fe65b6dce12f8f3de84d604f2e786da9c85c275
imMilanPatel <milanpatel3116@gmail.com>
2024-02-07 10:17:59 +0530

    Version 1 started work

---
+++ Strings basics.py
@@ -0,0 +1,12 @@
+'''
+This file contains basic files to learn and practise about strings.
+Taken from: https://github.com/inspirezonetech/TeachMePythonLikeIm5/blob/main/variables/strings.py
+Solutions will be based on the challenges mentioned in the above repo only.
+
+'''
+
+# 1. Create a string & print out each character individually.
+# 2. Create a string & find the length of it.
+# 3. Print a string with different predefined variables in it.
+# 4. Write a program that adds 'ing' to every string.
+# 5. Write a program that replaces every 's' in a string with a '$'
