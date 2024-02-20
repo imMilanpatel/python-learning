aa965ed8c83fdaaf9a2f74a23d5ffbfe579a697a
imMilanPatel <milanpatel3116@gmail.com>
2024-02-20 17:37:38 +0530

    removed few lines in webpage access file

--- Automation/Selenium/OrangeHRM/Test Scripts/test_webpage_access.py
+++ Automation/Selenium/OrangeHRM/Test Scripts/test_webpage_access.py
@@ -1,19 +1,8 @@
 # This file will test out the access to the webpage of the site under test
 
 from selenium import webdriver
-from Utilities import Website_Config
-import time
 
 class TestSiteAccess:
-
-    def setup(self):
-        driver = webdriver.Chrome()
-        return driver
-    
-    def open_app(self):
-        self.driver.get(Website_Config.Credentials.site_link) 
-        time.wait(5)
-
-    def teardown(self):
-        self.driver.quit() 
+    # To be done
+    pass
               
