6e6593401d4f797fb4415b620c8cb71cab7001d2
imMilanPatel <milanpatel3116@gmail.com>
2024-02-20 16:41:03 +0530

    minor updates

--- Automation/Selenium/OrangeHRM/Test Scripts/test_webpage_access.py
+++ Automation/Selenium/OrangeHRM/Test Scripts/test_webpage_access.py
@@ -1,1 +1,9 @@
 # This file will test out the access to the webpage of the site under test
+
+import selenium
+from Utilities import Website_Config
+
+class TestSiteAccess:
+
+    def setup(Self):
+        
