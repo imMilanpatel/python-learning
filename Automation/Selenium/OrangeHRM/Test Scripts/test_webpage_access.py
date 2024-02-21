114522c971bb2b6f6abde3d31688ce7a0629ec9e
imMilanPatel <milanpatel3116@gmail.com>
2024-02-21 09:30:01 +0530

    basic selenium template file created

--- Automation/Selenium/OrangeHRM/Test Scripts/test_webpage_access.py
+++ Automation/Selenium/OrangeHRM/Test Scripts/test_webpage_access.py
@@ -1,7 +1,28 @@
 # This file will test out the access to the webpage of the site under test
 
 from selenium import webdriver
+from selenium import webdriver
 
+class TestSiteAccess:
+    def __init__(self):
+        # Set the path of the chromedriver executable
+        self.driver = webdriver.Chrome()
+
+    def visit_url(self):
+        # Visit the URL
+        self.driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    def close_browser(self):
+        # Close the browser
+        self.driver.close()
+
+    def run(self):
+        self.visit_url()
+        self.close_browser()
+
+if __name__ == '__main__':
+    test_site_access = TestSiteAccess()
+    test_site_access.run()
 class TestSiteAccess:
     # To be done
     pass
