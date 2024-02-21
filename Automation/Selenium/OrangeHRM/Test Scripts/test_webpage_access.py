de1a2fe78117c3424e6b932b9e2237824384f6af
imMilanPatel <milanpatel3116@gmail.com>
2024-02-21 09:37:39 +0530

    TODO checkpoints

--- Automation/Selenium/OrangeHRM/Test Scripts/test_webpage_access.py
+++ Automation/Selenium/OrangeHRM/Test Scripts/test_webpage_access.py
@@ -1,17 +1,23 @@
 # This file will test out the access to the webpage of the site under test
 
-from selenium import webdriver
+# Imports
 from selenium import webdriver
 
+# Test Class
 class TestSiteAccess:
+
+    # To initialize the chrome browser driver
     def __init__(self):
         # Set the path of the chromedriver executable
         self.driver = webdriver.Chrome()
 
+    #FIXME: This function needs to get variable from other file
+    # To visit site
     def visit_url(self):
         # Visit the URL
         self.driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
+    # To close 
     def close_browser(self):
         # Close the browser
         self.driver.close()
--- Automation/Selenium/OrangeHRM/Utilities/Website_Config.py
+++ Automation/Selenium/OrangeHRM/Utilities/Website_Config.py
@@ -2,6 +2,7 @@
 
 class Credentials:
     
+    #TODO: This file needs to be updated with XPATHS of the site objects...
     def site_link(self):
         site_url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
         return site_url
