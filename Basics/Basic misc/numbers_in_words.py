ff9ad8955a602b73aa86fc407d01544588a8891e
imMilanPatel <milanpatel3116@gmail.com>
2024-02-26 11:32:51 +0530

    minor changes done

---
+++ Basics/Basic misc/numbers_in_words.py
@@ -0,0 +1,10 @@
+# Program to convert numbers in wordings
+# Eg: 10,500 --> Ten Thousand Five Hundred
+
+def number_to_indian_words(num):
+    pass
+
+# Example
+input_number = 112569
+output_words = number_to_indian_words(input_number)
+print(f"{input_number:,} is written as {output_words}")
