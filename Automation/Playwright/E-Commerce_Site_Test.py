71dcc7f48764411cea9334d94cbb94243f3661f4
imMilanPatel <milanpatel3116@gmail.com>
2024-02-28 15:09:12 +0530

    E-commerce testing file for practise added

---
+++ Automation/Playwright/E-Commerce_Site_Test.py
@@ -0,0 +1,6 @@
+# TODO: Implement the below functionality one by one
+# def test_amazon_login(browser):
+# def test_product_checkout(browser):
+# def test_search_functionality(browser):
+# def test_wishlist_functionality(browser):
+
