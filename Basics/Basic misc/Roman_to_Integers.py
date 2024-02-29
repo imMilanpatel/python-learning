fa666bff01382658a455130936586a041dc022b2
imMilanPatel <milanpatel3116@gmail.com>
2024-02-29 17:38:45 +0530

    Code implemented ! But need to fix the code as it has some issues

--- Basics/Basic misc/Roman_to_Integers.py
+++ Basics/Basic misc/Roman_to_Integers.py
@@ -5,14 +5,43 @@ Explanation --> III == 3
 
 '''
 
+# Roman numbers equivalents in Integers
+romans_numerics = {
+    "I":1,
+    "V":5,
+    "X":10,
+    "L":50,
+    "C":100,
+    "D":500,
+    "M":1000,
+}
 
+# Function with the actual logic
 def roman_numbers_to_integers(Roman_Number):
     '''
     This method will convert Roman Numbers to Integers.
     Please provide your inputs in Roman Numerics only.
+
     '''
-    #TODO: Implement the logic
-    return
+    total = 0
+
+    for input_charcters in range(len(Roman_Number)):
+        try:
+            curr_index = input_charcters
+            curr_value = romans_numerics[Roman_Number[curr_index]]
+            next_value = romans_numerics[Roman_Number[curr_index+1]]
+            print(curr_value)
+            print(next_value)
+
+            if curr_value < next_value:
+                total -= curr_value
+            else:
+                total += curr_value
+
+        except IndexError:
+            pass
+        
+    return print(total)
 
 # Input
 Roman_num = input("Enter the Roman Number: ")
