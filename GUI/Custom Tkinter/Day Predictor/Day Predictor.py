a938ace4df7f28bf9d1d05ab23b0789877c92cbb
imMilanPatel <milanpatel3116@gmail.com>
2024-03-06 13:08:12 +0530

    Removed formatting as it had lot of bugs

--- GUI/Custom Tkinter/Day Predictor/Day Predictor.py
+++ GUI/Custom Tkinter/Day Predictor/Day Predictor.py
@@ -24,27 +24,7 @@ def find_button_click():
 # Clear button functionality
 def clear_button_click():
     entry.delete(0, customtkinter.END)
-
-def format_date(entry):
-    current_text = entry.get()
-
-    # Remove any non-digit characters
-    formatted_text = ''.join(c for c in current_text if c.isdigit())
-
-    # Format the date with slashes
-    if len(formatted_text) >= 2:
-        formatted_text = formatted_text[:2] + '/' + formatted_text[2:]
-    if len(formatted_text) >= 5:
-        formatted_text = formatted_text[:5] + '/' + formatted_text[5:]
-
-    # Limit the year to a maximum of four characters
-    if len(formatted_text) > 9:
-        formatted_text = formatted_text[:9]
-
-    entry.delete(0, customtkinter.END)
-    entry.insert(0, formatted_text)
-
-
+    
 #######################################################################
 
 # Load the image
@@ -63,9 +43,6 @@ entry_label.place(x=125, y=60)
 entry = customtkinter.CTkEntry(gui)
 entry.place(x=125, y=100)
 
-# Bind the format_date function to the key press event
-entry.bind('<Key>', lambda event: format_date(entry))
-
 # Create and place the "Find!" button
 find_button = customtkinter.CTkButton(gui, text="Find!", command=find_button_click)
 find_button.place(x= 128, y = 155)
@@ -73,7 +50,6 @@ find_button.place(x= 128, y = 155)
 # Create and place the "Clear" button
 clear_button = customtkinter.CTkButton(gui, text="Clear", command=clear_button_click)
 clear_button.place(x= 128, y= 190)
-
 
 # Driver code
 gui.mainloop()
