428ce817f15e8dc8c3887dac92d3ae74e5e0487b
imMilanPatel <milanpatel3116@gmail.com>
2024-03-08 14:41:28 +0530

    Perfect number logic implementation

---
+++ Basics/Basic misc/Perfect_Number.py
@@ -0,0 +1,6 @@
+'''
+File will calculate perfect number but only upto 
+a limit as it might consume huge computer resource to get a higher perfect number
+'''
+
+input_number = int(input("Enter the number: "))
