49efe5d5d04f3868659bbf18809a6ccf9ef0aac0
unknown <milanpatel3116@gmail.com>
2024-03-12 09:52:01 +0530

    Logic implemented

--- Basics/Basic misc/Power_Of_Four.py
+++ Basics/Basic misc/Power_Of_Four.py
@@ -1,3 +1,15 @@
 # This file will check whether the number given as input is a power of four or not?
 # Eg: Input = 16 output = True that is 16 = 4^2
 # An integer n is a power of 4 if there exists an integer x such that n == 4^x
+
+# Function definition
+def power_of_four(number) -> bool:
+    ''' Check if n is a positive integer and n is a power of 4 '''
+    if number > 0 and (number & (number - 1)) == 0 and number.bit_length() % 2 == 1:
+        return True
+    else:
+        return False
+    
+# User input
+input_number = int(input("Enter the number: "))
+print(power_of_four(input_number))
