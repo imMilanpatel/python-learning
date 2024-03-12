3d2dde1da743479c4ed75139d0e73b82c4ac7a73
unknown <milanpatel3116@gmail.com>
2024-03-12 10:33:48 +0530

    Logic implemented

--- Basics/Basic misc/Print_Short_Name.py
+++ Basics/Basic misc/Print_Short_Name.py
@@ -1,2 +1,16 @@
 # Input: Milan Ravi Patel
 # Output: M.R Patel
+
+# Function definition
+def short_name(firstname, middlename, lastname)-> str:
+    output = firstname[0] + "." + middlename[0] + " " + lastname
+    return output
+    
+
+
+# Input
+first_name = input("Enter your First Name: ")
+middle_name = input("Enter your Middle Name: ")
+last_name = input("Enter your Last Name: ")
+results = short_name(first_name, middle_name, last_name)
+print(results)
