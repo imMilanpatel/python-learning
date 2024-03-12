462b1de9765fcc1975221419f0bd4d473f5c132a
unknown <milanpatel3116@gmail.com>
2024-03-12 11:17:03 +0530

    Basic logic implemented

--- Games/Basic/QA JSON/QA_Game.py
+++ Games/Basic/QA JSON/QA_Game.py
@@ -1,1 +1,27 @@
 # This python file will read the Questions from JSON file and compare it with JSON value and print the result.
+import json
+
+# Open the JSON file
+with open('python-learning\Games\Basic\QA JSON\QA.json', 'r') as file:
+    # Load the JSON content
+    data = json.load(file)
+
+# Access the questions
+questions = data["questions"]
+
+# Game
+game_status = True
+while game_status:
+    total = 0
+    for qna in questions:
+        print("Question", qna["question"])
+        answer = input("Your answer: ")
+        if answer == qna["answer"]:
+            print("That's correct !")
+            total += 1
+            print(f"Your Current Score is: {total}")
+        else:
+            print("INCORRECT :(")
+
+    print(f"Final Score: {total}")
+    game_status = False
