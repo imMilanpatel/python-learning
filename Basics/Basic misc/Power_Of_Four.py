cc18e74f2e24442f348f7cfba7677e8d855dbca5
unknown <milanpatel3116@gmail.com>
2024-03-12 09:23:07 +0530

    problem statement

---
+++ Basics/Basic misc/Power_Of_Four.py
@@ -0,0 +1,3 @@
+# This file will check whether the number given as input is a power of four or not?
+# Eg: Input = 16 output = True that is 16 = 4^2
+# An integer n is a power of 4 if there exists an integer x such that n == 4^x
