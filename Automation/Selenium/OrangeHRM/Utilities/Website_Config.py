da2efe53008e71ecce18afecf8290e3143806994
unknown <milanpatel3116@gmail.com>
2024-03-13 16:48:20 +0530

    removed few things

--- Automation/Selenium/OrangeHRM/Utilities/Website_Config.py
+++
@@ -1,8 +0,0 @@
-# This file contains website which is to be tested related data
-
-class Credentials:
-    
-    #TODO: This file needs to be updated with XPATHS of the site objects...
-    def site_link(self):
-        site_url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-        return site_url
