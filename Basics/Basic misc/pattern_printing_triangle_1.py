81b7026e857fb5fe67067f68d52ab541b2d8a0f0
unknown <milanpatel3116@gmail.com>
2024-03-19 11:20:58 +0530

    logic implementation

--- Basics/Basic misc/pattern_printing_triangle_1.py
+++ Basics/Basic misc/pattern_printing_triangle_1.py
@@ -24,8 +24,14 @@
 |    *
 |  *
 
-
-
-
-
 """
+
+# Constants
+lines_to_print = 8
+spaces = 0
+
+for line in range(1,lines_to_print + 1):
+    print("|", " " * 2 * line, "*")
+    spaces += 2
+    print(spaces)
+    
