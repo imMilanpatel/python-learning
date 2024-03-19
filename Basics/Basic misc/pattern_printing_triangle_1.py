85e3789ab23842c9af349c1a5d6d110e93bc8ecc
unknown <milanpatel3116@gmail.com>
2024-03-19 11:30:16 +0530

    re-working on the actual logic

--- Basics/Basic misc/pattern_printing_triangle_1.py
+++ Basics/Basic misc/pattern_printing_triangle_1.py
@@ -29,9 +29,4 @@
 # Constants
 lines_to_print = 8
 spaces = 0
-
-for line in range(1,lines_to_print + 1):
-    print("|", " " * 2 * line, "*")
-    spaces += 2
-    print(spaces)
-    
+maximum_space = 8
