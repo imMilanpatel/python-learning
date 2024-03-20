2622e8b359a4f9e0fe6dfcd75ef162a8743989fb
unknown <milanpatel3116@gmail.com>
2024-03-20 16:40:01 +0530

    easy way, which is not recommended

--- Basics/Basic misc/pattern_printing_triangle_2.py
+++ Basics/Basic misc/pattern_printing_triangle_2.py
@@ -6,5 +6,13 @@ This program will print the following pattern,
     * * * * *
   * * * * * * *
 * * * * * * * * * 
-      
+
 '''
+
+lines = 1
+for _ in range(lines):
+    print(" " * 8, "*")
+    print(" " * 6, "*" * 5)
+    print(" " * 4, "*" * 10)
+    print(" " * 2, "*" * 14)
+    print("*" * 20)
