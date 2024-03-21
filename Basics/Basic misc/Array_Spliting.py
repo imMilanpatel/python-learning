f183101bcea81ed19bfde58805f859fb7f29f9d9
unknown <milanpatel3116@gmail.com>
2024-03-21 09:51:35 +0530

    minor code refactoring

--- Basics/Basic misc/Array_Spliting.py
+++ Basics/Basic misc/Array_Spliting.py
@@ -9,21 +9,23 @@ split_array_2 = [1,2,4]
 
 """
 
-input_array = [1,1,2,2,3,4,5,3]
+# Logic
+def array_split(input_array, array_1, array_2):
+    for number in input_array:
+        if number not in array_1 and len(array_1) != 0.5 * len(input_array):
+            array_1.append(number)
+        else:
+            array_2.append(number)
 
+
+# Inputs
+input_array = [1,1,2,2,3,4]
 split_array_1 = []
-
 split_array_2 = []
 
-#########################################
+# Function call
+array_split(input_array, split_array_1, split_array_2)
 
-for number in input_array:
-    if number not in split_array_1 and len(split_array_1) != 0.5 * len(input_array):
-        split_array_1.append(number)
-    else:
-        split_array_2.append(number)
-    
-        
-
+# Output verification
 print(f"split array 1 = {split_array_1}")
 print(f"split array 2 = {split_array_2}")
