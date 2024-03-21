f54dcdba0bfe8ffe7b52161aa78d409220f048ab
unknown <milanpatel3116@gmail.com>
2024-03-21 09:47:24 +0530

    final solution

--- Basics/Basic misc/Array_Spliting.py
+++ Basics/Basic misc/Array_Spliting.py
@@ -9,18 +9,21 @@ split_array_2 = [1,2,4]
 
 """
 
-input_array = [1,1,2,2,3,4]
+input_array = [1,1,2,2,3,4,5,3]
 
 split_array_1 = []
 
 split_array_2 = []
 
-#############
+#########################################
+
 for number in input_array:
-    if number not in split_array_1:
+    if number not in split_array_1 and len(split_array_1) != 0.5 * len(input_array):
         split_array_1.append(number)
     else:
         split_array_2.append(number)
+    
+        
 
 print(f"split array 1 = {split_array_1}")
 print(f"split array 2 = {split_array_2}")
