111ca94d52f1f7e61b1ec290176b7f98654e1e42
unknown <milanpatel3116@gmail.com>
2024-03-22 09:30:01 +0530

    problem statement created

---
+++ Basics/strings/final_word_count.py
@@ -0,0 +1,11 @@
+'''
+This file will calculate the final word count in the given string.
+
+Example:
+
+string_input = "Hello World"
+
+output = 5 
+(since word "World" has 5 characters, excluding spaces)
+
+'''
