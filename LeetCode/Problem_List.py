66a5daf10f8d9c50c17c251554da830b3bb80536
unknown <milanpatel3116@gmail.com>
2024-03-25 17:05:50 +0530

    temp solution

--- LeetCode/Problem_List.py
+++ LeetCode/Problem_List.py
@@ -5,22 +5,51 @@ Explanation: 342 + 465 = 807.
 
 '''
 
-list_one = [2,4,3][::-1]
-list_two = [5,6,4][::-1]
-                #  3 4 2 + 
-                #  4 6 5
-expected_output = [7,0,8] 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-output = []
-for index_of_number in range(len(list_one)):
-    element = list_one[index_of_number] + list_two[index_of_number]
-    if element < 10:
-        output.append(element)
-    else:
-        element_0th = element[0]
-        element_1st = element[1]
+def addTwoNumbers(l1, l2):
+    dummy = ListNode()  # Dummy node to simplify the code
+    current = dummy
+    carry = 0
 
+    while l1 or l2 or carry:
+        # Calculate sum and carry
+        sum_val = carry
+        if l1:
+            sum_val += l1.val
+            l1 = l1.next
+        if l2:
+            sum_val += l2.val
+            l2 = l2.next
+        
+        carry = sum_val // 10
+        sum_val %= 10
 
+        # Create new node with the sum and append it to the result linked list
+        current.next = ListNode(sum_val)
+        current = current.next
 
-  
-print(output)
+    return dummy.next
+
+# Helper function to print the linked list
+def printLinkedList(head):
+    current = head
+    while current:
+        print(current.val, end=" ")
+        current = current.next
+    print()
+
+# Test cases
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+result = addTwoNumbers(l1, l2)
+printLinkedList(result)
