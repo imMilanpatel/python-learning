977180f6681795980405ab10102315cdc9533d16
unknown <milanpatel3116@gmail.com>
2024-03-27 11:18:16 +0530

    Logic Implemented

--- LeetCode/Merge_Two_Sorted_list.py
+++ LeetCode/Merge_Two_Sorted_list.py
@@ -1,23 +1,35 @@
 '''
-
 Input: 
 
 list1 = [1,2,4] 
 list2 = [1,3,4]
 
 Output: [1,1,2,3,4,4]
-
 '''
-list1 = [1,2,4] 
-list2 = [1,3,4]
-output = []
 
-for number in list1:
-    output.append(number)
+def merge_sorted_lists(list1, list2):
+    result = []
+    i = j = 0
     
-for number2 in list2:
-    output.append(number2)
+    while i < len(list1) or j < len(list2):
+        if j == len(list2) or (i < len(list1) and list1[i] < list2[j]):
+            result.append(list1[i])
+            i += 1
+        else:
+            result.append(list2[j])
+            j += 1
+    
+    return result
 
-output.sort()
+# Example usage:
+list1 = [1, 2, 4]
+list2 = [1, 3, 4]
+print(merge_sorted_lists(list1, list2))  # Output: [1, 1, 2, 3, 4, 4]
 
-print(output)
+list1 = []
+list2 = []
+print(merge_sorted_lists(list1, list2))  # Output: []
+
+list1 = []
+list2 = [0]
+print(merge_sorted_lists(list1, list2))  # Output: [0]
