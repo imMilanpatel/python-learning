383aa16fe2f56eaa59975c64fb9813aa679b3f76
unknown <milanpatel3116@gmail.com>
2024-03-27 11:15:09 +0530

    very basic logic implemented

--- LeetCode/Merge_Two_Sorted_list.py
+++ LeetCode/Merge_Two_Sorted_list.py
@@ -10,5 +10,14 @@ Output: [1,1,2,3,4,4]
 '''
 list1 = [1,2,4] 
 list2 = [1,3,4]
-ouput = []
+output = []
 
+for number in list1:
+    output.append(number)
+    
+for number2 in list2:
+    output.append(number2)
+
+output.sort()
+
+print(output)
