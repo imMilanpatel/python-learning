8d61d24a9a4369d12808f19923515f0609e6095f
unknown <milanpatel3116@gmail.com>
2024-04-13 10:52:47 +0530

    template files created

---
+++ Automation/Playwright/SauceDemo/POM/login_page_elements.py
@@ -0,0 +1,5 @@
+# UI Elements on the Login Page of SauceDemo.com
+
+username_input_box = ''
+password_input_box = ''
+login_button = ''
---
+++ Automation/Playwright/SauceDemo/Utilities/credentials.py
@@ -0,0 +1,15 @@
+# Website related credentials
+
+# website link
+url = "https://www.saucedemo.com/"
+
+
+
+# Login Credentials
+
+# Username list
+accepted_users = ['standard_user','locked_out_user','problem_user',
+                  'performance_glitch_user','error_user','visual_user']
+
+# Password
+password = 'secret_sauce'
