ad4c636d323cfff55c1a1f72f0e86f332d1d1270
unknown <milanpatel3116@gmail.com>
2024-04-13 11:30:06 +0530

    refactoring done

--- Automation/Playwright/SauceDemo/tests/test_login.py
+++ Automation/Playwright/SauceDemo/tests/test_login.py
@@ -21,18 +21,22 @@ def page(browser):
     yield page
     page.close()
 
-######################################## 
-
-# Actual tests
-
-def test_login(page):
-    page.goto(credentials.url)
-    page.wait_for_load_state('networkidle')
-
-    page.locator(login_page_elements.username_input_box).fill(credentials.accepted_users[0])
+# Username and password field input boxes and credentials passing template function for resuability
+def template(page,index):
+    page.locator(login_page_elements.username_input_box).fill(credentials.accepted_users[index])
     time.sleep(2)
     page.locator(login_page_elements.password_input_box).fill(credentials.password)
     time.sleep(2)
     page.locator(login_page_elements.login_button).click()
     time.sleep(2)
+
+######################################## 
+
+# Actual tests
+def test_standard_user_login(page):
+    page.goto(credentials.url)
+    page.wait_for_load_state('networkidle')
+
+    template(page, index=0)
+    
     page.wait_for_load_state('networkidle')
